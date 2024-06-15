59df09eeefbd290f70e077f9a3588e2cfda11645
Raghavk1 <raghavkhanna0011@gmail.com>
2024-06-15 11:56:23 +0530

    [Added]MERN Project

---
+++ Library-Store-MERN-Stack-main/README.md
@@ -0,0 +1,13 @@
+# Library Store MERN stack project
+
+# How to Set up -->
+1. Clone/Download the Repo from github.
+2. cd Backend
+3. npm install (install the required modules)
+4. npm run dev to run the app
+5. Before running the app change the MongoDb cluster config inside config.js 
+6. Start the server
+7. cd to frontend
+8. npm install 
+9. npm run dev 
+
---
+++ Library-Store-MERN-Stack-main/backend/config.js
@@ -0,0 +1,8 @@
+export const PORT = 5555;
+
+// export const mongoDBURL =
+//   'mongodb+srv://root:root@books-store-mern.c7ba8z1.mongodb.net/books-collection?retryWrites=true&w=majority';
+
+export const mongoDBURL = "mongodb+srv://fireraghav1:Raghavk1%40@cluster0.ryarib3.mongodb.net/books-collection?retryWrites=true&w=majority&appName=Cluster0"
+// Please create a free database for yourself.
+// This database will be deleted after tutorial
